f11f8375d77084f9ebbeaa586a7036a57cd8670a
Gabriela Hensel Ciudrowski <106684514+GabiHensel@users.noreply.github.com>
2023-08-16 10:02:17 -0300

    header gabi

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,5 @@
-import Head from 'next/head'
 import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <><Component {...pageProps} /><Head><title>IndHUGE</title></Head></> 
-  
-  
+  return <Component {...pageProps} />
 }
