c44c640b24422f52a70881e26e58d5d4b38baaf7
Gabriela Hensel Ciudrowski <106684514+GabiHensel@users.noreply.github.com>
2023-08-18 09:32:57 -0300

    inocio envio de email

--- components/formulario/index.jsx
+++ components/formulario/index.jsx
@@ -1,6 +1,15 @@
+import axios from "axios"
 import Styles from "./style.module.scss"
 
 const Formulario = () => {
+
+    const SendEmail =() =>{
+        axios
+        .post("/api/sendEmail")
+        .then(()=>console.log("deu certo"))
+        .catch(()=>console.log("deu erro"));
+    }
+
     return <div className={Styles.container} id="formulario">
         <form>
             <h2>ENTRE EM CONTATO E TRANSFORME A SUA PRODUÇÃO</h2>
@@ -9,8 +18,11 @@ const Formulario = () => {
             <input type="text" placeholder=" TELEFONE" pattern="^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$" required/>
             <input type="text" className={Styles.msg} placeholder=" MENSAGEM" required/>
             <input type="submit" placeholder=" ENVIAR"/>
+            <button>teste</button>
         </form>
-    </div>
+
+        <button onClick={()=>SendEmail}><label>ENTRE EM CONTATO</label></button>
+        </div>
 }
 
 export default Formulario
